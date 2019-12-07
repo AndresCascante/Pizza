03f179b5e2032c3bcfc902d37c46f44010a0d2b3
AndresCascante <ancas896@gmail.com>
2019-12-07 12:24:28 -0600

    Changes 2, base for Sign in and sign up

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
+from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
+    url(r'^signup/$', core_views.signup, name='signup'),
 ]
